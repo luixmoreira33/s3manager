493614bfac9070dea0a1cf06bb5124fa49b0b464
luixmoreira33 <eu@luizalmeida.me>
2025-09-04 12:29:17 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -71,7 +71,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles" # Descomente para produção
+STATIC_ROOT = BASE_DIR / "staticfiles" # Descomente para produção
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
