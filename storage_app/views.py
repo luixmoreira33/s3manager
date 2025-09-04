686184f54d1db89799ffaf40cd28180b52fe1043
luixmoreira33 <eu@luizalmeida.me>
2025-09-04 12:22:06 -0300

    Update views.py

--- storage_app/views.py
+++ storage_app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.contrib.auth.decorators import login_required, staff_member_required
+from django.contrib.auth.decorators import login_required
+from django.contrib.admin.views.decorators import staff_member_required
 from django.contrib import messages
 from django.http import Http404, HttpResponseBadRequest
 from urllib.parse import unquote
