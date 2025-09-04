39ae6548f2236f46eed7ae603571997607f4c465
luixmoreira33 <eu@luizalmeida.me>
2025-09-04 16:39:44 -0300

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -2,6 +2,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
+
+admin.site.site_header = "S3 File Manager"
+admin.site.site_title = "Painel S3 Manager"
+admin.site.index_title = "Bem-vindo ao Gerenciador de Arquivos"
+
+
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("files/", include("storage_app.urls", namespace="storage_app")),
